2c0cf17945a58ea79838c71620207d83d06cf285
test1234 <147180460+githubTest8246@users.noreply.github.com>
2024-08-17 22:17:41 +0300

    Update php.php

--- php.php
+++ php.php
@@ -1,2 +1,19 @@
 <?php
-    echo "hello";
+
+    $method = $_SERVER["REQUEST_MEHTHOD"];
+
+    if ($method == "GET"){
+        $response = [
+            "message" => "mission retireved",
+            "success" => true
+            ]
+        }else{
+            http_response_code(405); // Method Not Allowed
+            $response = [
+                "message" => "Method not allowed",
+                "success" => false
+            ];
+        }
+    echo json_encode($response);
+    break;
+    
